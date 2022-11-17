af95fcc89dd71a040223c5f468673b565ead5ba2
dogustuluk <dogus.tuluk@gmail.com>
2022-11-17 17:26:22 +0300

    client credentials - 5

---
+++ IdentityServer.API1/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using IdentityServer.API1.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace IdentityServer.API1.Controllers
+{
+    [Route("api/[controller]/[action]")] //action ile metot isimleriyle endpoint'e ulaşmak istiyoruz.
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        //-->   api/product/getproducts
+        [Authorize]
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            var productList = new List<Product>() { 
+                new Product { Id = 1, Name = "kalem 1", Price = 100, Stock = 100 },
+                new Product { Id = 2, Name = "kalem 2", Price = 200, Stock = 200 }, 
+                new Product { Id = 3, Name = "kalem 3", Price = 300, Stock = 300 },
+                new Product { Id = 4, Name = "kalem 4", Price = 400, Stock = 400 }, 
+                new Product { Id = 5, Name = "kalem 5", Price = 500, Stock = 500 } 
+            };
+            return Ok(productList);
+        }
+    }
+}
---
+++ IdentityServer.API2/Controllers/PicturesController.cs
@@ -0,0 +1,28 @@
+﻿using IdentityServer.API2.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace IdentityServer.API2.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PicturesController : ControllerBase
+    {
+        [Authorize]
+        [HttpGet]
+        public IActionResult GetPicture()
+        {
+            var pictures = new List<Picture>()
+            {
+                new Picture() {Id=1,Name="resim 1", Url="resim1.jpg"},
+                new Picture() {Id=2,Name="resim 2", Url="resim2.jpg"},
+                new Picture() {Id=3,Name="resim 3", Url="resim3.jpg"},
+                new Picture() {Id=4,Name="resim 4", Url="resim4.jpg"},
+                new Picture() {Id=5,Name="resim 5", Url="resim5.jpg"}
+            };
+            return Ok(pictures);
+        }
+    }
+}
